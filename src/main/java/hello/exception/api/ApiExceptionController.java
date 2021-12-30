534d56109dc0a519180539e5647ff38005f920cf
HongDae <fholng@outlook.com>
2021-12-30 18:24:16 +0900

    responsestatusexception

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,14 +1,17 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -16,13 +19,20 @@ public class ApiExceptionController {
 
     @GetMapping(value = "/api/members/{id}")
     public MemberDto getMember(@PathVariable String id) {
-        if (id.equals("ex"))
-            throw new RuntimeException("잘못된 사용자");
-        if (id.equals("bad"))
-            throw new IllegalArgumentException("잘못된 입력 값");
-        if (id.equals("user-ex"))
-            throw new UserException("사용자 오류");
+        if (id.equals("ex")) throw new RuntimeException("잘못된 사용자");
+        if (id.equals("bad")) throw new IllegalArgumentException("잘못된 입력 값");
+        if (id.equals("user-ex")) throw new UserException("사용자 오류");
         return new MemberDto(id, "Hello, " + id);
+    }
+
+    @GetMapping(value = "/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    @GetMapping(value = "/api/response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
     }
 
     @Data
