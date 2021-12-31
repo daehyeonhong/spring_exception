145bf7b8b18dbf2e4cc1814f95be01da90796d6d
hongdae <fholng@outlook.com>
2021-12-31 22:22:08 +0900

    ex

--- src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
+++ src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
@@ -18,16 +18,16 @@ public class UserHandlerExceptionResolver implements HandlerExceptionResolver {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) {
         try {
-            if (ex instanceof UserException) {
+            if (exception instanceof UserException) {
                 log.info("UserException resolver to 400");
                 String acceptHeader = request.getHeader("accept");
                 response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 if ("application/json".equals(acceptHeader)) {
                     Map<String, Object> errorResult = new HashMap<>();
-                    errorResult.put("ex", ex.getClass());
-                    errorResult.put("message", ex.getMessage());
+                    errorResult.put("ex", exception.getClass());
+                    errorResult.put("message", exception.getMessage());
                     String result = objectMapper.writeValueAsString(errorResult);
                     response.setContentType(MediaType.APPLICATION_JSON_VALUE);
                     response.setCharacterEncoding("UTF-8");
@@ -37,8 +37,8 @@ public class UserHandlerExceptionResolver implements HandlerExceptionResolver {
                     return new ModelAndView("/error/500");
                 }
             }
-        } catch (IOException exception) {
-            log.error("resolver ex", exception);
+        } catch (IOException ex) {
+            log.error("resolver exception", ex);
         }
         return null;
     }
